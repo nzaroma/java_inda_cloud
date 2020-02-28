c66a45f1d2e4a61e23f7e0ed7a4c9d91975a4634
Roman_Vinokurov <roman_vinokurov@epam.com>
2020-02-28 17:21:56 +0300

    kafka 2

--- pc/src/main/java/org/nipu/pc/PcApplication.java
+++ pc/src/main/java/org/nipu/pc/PcApplication.java
@@ -1,15 +1,33 @@
 package org.nipu.pc;
 
+import org.nipu.po.order.ProductOrder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.messaging.Message;
+
+import java.util.List;
 
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableBinding(Sink.class)
 public class PcApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PcApplication.class, args);
+    }
+
+    @StreamListener(target = Sink.INPUT)
+    public void handleMessage(List<ProductOrder> message) {
+        System.out.println(message);
+    }
+
+    @StreamListener(target = Sink.INPUT)
+    public void handleMessage2(Object message) {
+        System.out.println(message);
     }
 }
--- po/src/main/java/org/nipu/po/PoApplication.java
+++ po/src/main/java/org/nipu/po/PoApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Sink;
 
 @SpringBootApplication
 @EnableFeignClients
